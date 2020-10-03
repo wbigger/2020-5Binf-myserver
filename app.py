cdfa39a8c86804c055f0c0ca307115ca3d577257
Claudio <wbigger@gmail.com>
2020-10-03 10:53:17 +0200

    add simple server

---
+++ app.py
@@ -0,0 +1,22 @@
+# La seguente istruzione importa la classe "Flask" dalla libreria "flask"
+# Notare che possiamo usare la libreria flask perché l'abbiamo già installata con pip3
+from flask import Flask
+
+# Istanzio l'oggetto della classe Flask
+# L'argomento è una qualsiasi stringa identificativa, nel nostro caso "marconi"
+# Notare che non dobbiamo dichiarare il tipo, perché viene "inferito" dal compilatore
+app = Flask("marconi")
+
+# Le istruzioni che cominciano con @ si chiamano "annotazioni"
+# Ci dicono qualcosa su quello che segue
+# In particolare l'annotazione @app.route ci dice che la funzione che segue
+# deve rispondere ad una particolare rotta, in questo caso "/" che è la rotta di default
+@app.route('/')
+# Definiamo una funzione con la parola chiave "def"
+# Anche in questo caso il tipo di ritorno della funzione viene inferito
+def hello():
+	return '<h1>ciao sono il prof</h1>'
+
+
+# Notare che le righe in python non richiedono il ; alla fine
+# Il contenuto delle funzioni deve essere indentato, perché non si usano le parentesi graffe
